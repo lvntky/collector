13d9271bf7af3932632519018eb949015006a1d2
lvntky <klevent1903@gmail.com>
2023-04-12 12:35:14 +0300

    memorry allocation implementation

--- src/memalloc.c
+++ src/memalloc.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <assert.h>
 #include <stdio.h>
+#include <string.h>
 
 typedef struct block_meta
 {
@@ -75,55 +76,105 @@ void *memalloc(size_t size)
 	return NULL;
       } else { // found free block
 	block->free = 0;
-	block->magic = 0x7777777;
+	block->magic = 0x77777777;
       }
     }
   }
+  // return next of the current allocated block
   return (block + 1);
 }
 
-// TEST
-typedef  struct node {
-  int data;
-  struct node * next;
-} node;
-node *ll_head = NULL;
-
-void insert_tail (int data) {
-  node *temp, *current;
-  temp = memalloc(sizeof(node));
-  temp ->data = data;
-  temp -> next = NULL;
-  if(ll_head == NULL) {
-    ll_head = temp;
-  }
-  else {
-    current = ll_head;
-    while(current->next != NULL) {
-      current = current -> next;
-    }
-    current -> next = temp;
-  }
+block_meta *return_last_allocated_chunk_adress(void *ptr)
+{
+  return (block_meta*) ptr -1;
 }
 
-void print_list() {
-  node* current = ll_head;
-  while(current != NULL) {
+// free need to set -> free
+void mem_free(void *ptr)
+{
+  if(!ptr) {
+    return;
+  }
+  block_meta *last_allocated_chunk = return_last_allocated_chunk_adress(ptr);
+  if(last_allocated_chunk != NULL) {
+    assert(last_allocated_chunk->free == 0);
+  }else {
+    last_allocated_chunk->free = 1;
+  }
+}
+// realloc should adjust the size of allocated chunk by malloc
+// If passed NULL just act like malloc
+// If passed previously allocated pointer
+//    free up the space, if request size is bigger than prev allocted size
+//    allocate more space, wipe the previously allocated space
+//    copy the content to new allocated bigger space
+void *mem_realloc(void* ptr, size_t size)
+{
+  if(ptr == NULL) {
+    return memalloc(size);
+  }
+  block_meta *last_allocated_chunk = return_last_allocated_chunk_adress(ptr);
+  if(last_allocated_chunk->size >= size) {
+    // we have enough space, nothing to do...
+    return ptr;
+  }
+  void *bigger_chunk;
+  bigger_chunk = memalloc(size);
+  if(!bigger_chunk) {
+    return NULL; // TODO: custom errno
+  }
+  memcpy(bigger_chunk,last_allocated_chunk, last_allocated_chunk->size);
+  mem_free(last_allocated_chunk);
+  return bigger_chunk;
+}
+
+// calloc - just wipe the memory before return pointer
+void *mem_calloc(size_t melem, size_t nelem) {
+  size_t size = melem * nelem;
+  void *ptr = memalloc(size);
+  memset(ptr, 0, size);
+  return ptr;
+}
+
+// TEST
+typedef struct node {
+  int data;
+  struct node* next;
+} node;
+node *head_linked = NULL;
+void insert_tail(int data) {
+  node *temp;
+  temp = memalloc(sizeof(node));
+  temp->data = data;
+  temp->next = NULL;
+  if(head_linked == NULL) {
+    head_linked = temp;
+  }
+  else {
+    node * current;
+    current = head_linked;
+    while(current->next != NULL) {
+      current = current->next;
+    }
+    current->next = temp;
+  }
+}
+void print_linked_list() {
+  node* current = head_linked;
+  while(current->next != NULL) {
     printf("%d --> ", current->data);
     current = current->next;
   }
   printf("NULL");
 }
-
-// DRIVER
 int main(int argc, char **argv)
 {
-  int input;
-  while(input != -1) {
+  int val = 0;
+  while(val != -1) {
     printf("data: ");
-    scanf("%d", &input);
-    insert_tail(input);
+    scanf("%d", &val);
+    insert_tail(val);
   }
-  print_list();
+  print_linked_list();
   return 0;
 }
